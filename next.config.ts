42a4ff39f4d70fe120e85704e835d12e9365e2bd
Milton Barrientos <miltonbarrientos2@gmail.com>
2024-12-09 17:47:11 -0600

    add image domain to Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["localhost", "xqoltwofgjyfdjsfdjib.supabase.co"],
+    domains: ["localhost", "xqoltwofgjyfdjsfdjib.supabase.co", "i.ibb.co"],
   },
   /* config options here */
 };
