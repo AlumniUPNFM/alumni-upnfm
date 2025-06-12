e9abee4d99b1ec2e720a227d54941b3c3eba3d54
crywhat <miltonbarrientos2@gmail.com>
2025-06-11 21:45:38 -0600

    feat: conditionally render notifications button in Header component based on user login status to enhance user experience

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -163,19 +163,21 @@ const Header = (
               text="Formación Continua"
               styleClass="w-full xl:w-max"
             />
-            <HeaderButton
-              isAnchor={true}
-              selected={isNotifications}
-              href="/notifications"
-              text="Notificaciones"
-              styleClass="w-full xl:w-max relative"
-            >
-              {unreadCount > 0 && (
-                <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
-                  {unreadCount}
-                </span>
-              )}
-            </HeaderButton>
+            {userLogged && (
+              <HeaderButton
+                isAnchor={true}
+                selected={isNotifications}
+                href="/notifications"
+                text="Notificaciones"
+                styleClass="w-full xl:w-max relative"
+              >
+                {unreadCount > 0 && (
+                  <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                    {unreadCount}
+                  </span>
+                )}
+              </HeaderButton>
+            )}
           </nav>
           {/* Perfil de usuario o botón de iniciar sesión */}
           {userLogged ? (
